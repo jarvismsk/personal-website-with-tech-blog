6970b65a2fb9e8336013d8b5451e3456f530268e
Irfan Khan <jarvis@Irfans-MacBook-Pro.local>
2023-08-08 21:36:18 +0530

    Fixed bug

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,16 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  try {
+    const date = parseISO(dateString);
+
+    if (!isNaN(date.getTime())) {
+      return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+    } else {
+      return <span>Invalid date</span>;
+    }
+  } catch (error) {
+    console.error('Error parsing date:', error);
+    return <span>Error parsing date</span>;
+  }
 }
