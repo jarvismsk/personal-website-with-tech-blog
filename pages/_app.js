518617d726a0d64f14fdd4f97b1151c6b6fa2228
Irfan Khan <122979963+jarvismsk@users.noreply.github.com>
2023-08-09 04:37:40 +0000

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/Styles/global.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
+     <Analytics />
   }
